42d52b722175669386dae9496c7911a7cdac6dba
zouyifeng <zouyifeng@aliyun.com>
2019-01-09 15:29:11 +0800

    入库时间对比

--- src/controllers/website.ts
+++ src/controllers/website.ts
@@ -3,6 +3,7 @@ import * as iconv from 'iconv-lite'
 import * as cheerio from 'cheerio'
 import { Movie } from '../entities/movie'
 import { MovieService } from '../services/movie'
+const logger = require('../log').logger('index.ts', 'warn')
 
 const listPageConfig: fetchConfig = {
   url: 'https://www.dytt8.net/', 
@@ -20,25 +21,26 @@ interface axiosResponseFunc {
   (res: AxiosResponse): void;
 }
 
+// 获取列表页信息
 const handleListPageResponse = (response: AxiosResponse) => {
   const str = iconv.decode(response.data, 'gb2312')
   const $ = cheerio.load(str, { decodeEntities: false })
   const $list = $('.co_area2').eq(2).find('tr')
   const length: number = $list.length
+  const now = new Date()
+  // 网站在当天发布前一天视频
+  const preDate = new Date(now.getTime() - 24 * 60 * 60 * 1000)
+  const preDateStr = `${preDate.getFullYear()}-${preDate.getMonth() + 1 < 10 ? '0' + (preDate.getMonth() + 1) : preDate.getMonth() + 1}-${preDate.getDate() < 10 ? '0' + preDate.getDate() : preDate.getDate()}`
   for (var i = 1; i < length; i++) {
     const url: string = $list.eq(i).find('a').eq(1).attr('href')
     const time: string = $list.eq(i).find('td font').html()
-    // movieList.push({ url, time })
-    // console.log(url)
-    const now = new Date()
-    // 网站在当天发布前一天视频
-    const nowStr = `${now.getFullYear()}-${now.getMonth() + 1 < 10 ? '0' + now.getMonth() + 1 : now.getMonth() + 1}-${now.getDate() - 1}`
-    if (nowStr === time) {
+    if (preDateStr === time) {
       fetch({ url: `https://www.dytt8.net${url}`, methods: 'get', responseType: 'arraybuffer'}, handleDetailPageResponse)
     }
   }
 }
 
+// 获取详情页信息
 const handleDetailPageResponse = (response: AxiosResponse) => {
   const str = iconv.decode(response.data, 'gb2312');
   const $ = cheerio.load(str, { decodeEntities: false })
@@ -71,6 +73,8 @@ const handleDetailPageResponse = (response: AxiosResponse) => {
   mv.create_time = nowStr;
 
   MovieService.newMovie(mv)
+  logger.info(`上架电影 ${mv.title}`)
+  logger.info(`上架日期 ${mv.publish_time}`)
 }
 
 const fetch = (config: fetchConfig, handleResponse: axiosResponseFunc) => {
