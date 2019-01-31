e936049b468da92b58af20ba49f1f8603b7e964e
zouyifeng <zouyifeng@aliyun.com>
2019-01-31 14:03:05 +0800

    添加入库loog

--- src/services/movie.ts
+++ src/services/movie.ts
@@ -2,12 +2,26 @@ import { injectable } from 'inversify';
 import { Movie } from '../entities/movie';
 import { getManager, getConnection, Equal } from 'typeorm';
 import { DBConnection } from '../DBConnection'
-
+const logger = require('../log').logger('services/movie.ts', 'warn')
 
 @injectable()
 export class MovieService {
   public static async newMovie(movie: Movie) {
     // getConnection('movie').getRepository('movie').save(movie);
+    logger.info(`INSERT INTO movie (
+      title, intro, cover, actor, country, publish_time, rate, create_time, download_url, type
+    ) VALUES (
+      "${movie.title}", 
+      "${movie.intro}", 
+      "${movie.cover}", 
+      "${movie.actor}", 
+      "${movie.country}", 
+      "${movie.publish_time}", 
+      "${movie.rate}", 
+      "${movie.create_time}", 
+      "${movie.download_url}",
+      "${movie.type}"
+    )`)
     getConnection('movie').query(`INSERT INTO movie (
       title, intro, cover, actor, country, publish_time, rate, create_time, download_url, type
     ) VALUES (
